3a9bfe8edc506f3da2000b9ff476e8fd8bb0d89d
hatake_kakashi <sandy.andryanto.mki@gmail.com>
2021-12-30 07:32:58 +0700

    updated

--- golang/part006/main.go
+++ golang/part006/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 
 	// var x [5]int
@@ -40,7 +38,16 @@ func main() {
 
 	// fmt.Println(x)
 
-	x := make([]float64, 5, 10)
-	fmt.Println(x)
+	// x := make([]float64, 5, 10)
+	// fmt.Println(x)
+
+	// x := [5]float64{98, 93, 77, 82, 83}
+
+	// var total float64 = 0
+	// for _, value := range x {
+	// 	total += value
+	// }
+
+	// fmt.Println(total / float64(len(x)))
 
 }
---
+++ golang/part007/main.go
@@ -0,0 +1,40 @@
+package main
+
+func main() {
+
+	// slice1 := []int{1, 2, 3}
+	// slice2 := make([]int, 2)
+	// copy(slice2, slice1)
+
+	// fmt.Println(slice1, slice2)
+
+	// x := make(map[string]int)
+	// x["key"] = 10
+	// fmt.Println(x["key"])
+
+	// x := make(map[int]int)
+	// x[1] = 10
+
+	// fmt.Println(x[1])
+
+	// elements := make(map[string]string)
+	// elements["A"] = "Bandung"
+	// elements["B"] = "Jakarta"
+
+	// fmt.Println(elements["B"])
+
+	// name, ok := elements["A"]
+	// fmt.Println(name, ok)
+
+	// if name, ok := elements["A"]; ok {
+	// 	fmt.Println(name, ok)
+	// }
+
+	elements := map[string]map[string]string{
+		"H" : map[string]string{
+			"name": "Hydrogen",
+			"state": "gas"
+		}
+	}
+
+}
